c6fb24dc1aadc46e8708bde8c677c5bbdb017a1d
mehlam52 <mehlan33hamid@gmail.com>
2022-05-22 23:38:18 +0530

    firist commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use('/posts', postRoutes);
 app.get('/', (req,res)=>{
   res.send('Hello to memories API');
 });
-
+//hello
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
