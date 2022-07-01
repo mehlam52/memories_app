f75de8ba1de8948936cd2d8ff6af0e469512584a
mehlam53 <mehlam.hamid@techbona.com>
2022-07-01 17:50:27 +0530

    image viewer

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
-
+// import { useNavigate } from 'react-router-dom';
 import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
+// import Box from '@mui/material/Box';
+// import Modal from '@mui/material/Modal';
+import Modal from 'react-bootstrap/Modal'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Post = ({ post, setCurrentId }) => {
+  const inputRef = React.useRef(null)
   const dispatch = useDispatch();
   const classes = useStyles();
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [imgs, setImg] = React.useState();
+  const [creator, setCreator] = React.useState();
+
 
   return (
     <Card className={classes.card}>
@@ -34,6 +59,34 @@ const Post = ({ post, setCurrentId }) => {
         <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
       </CardActions>
+
+      <button ref={inputRef} type="button" className="btn btn-light" data-toggle="modal" data-target="#exampleModal"
+        onClick={() => {
+          setImg(post.selectedFile)
+          setCreator(post.creator)
+          handleShow()
+        }}
+      >
+        View full Image
+      </button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Posted By -{creator}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <img style={{ width: '100%' }} src={imgs} />
+          </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+
+
+
     </Card>
   );
 };
